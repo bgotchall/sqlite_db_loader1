fe768b29941cd6d4f369d97aaef0178ef771bff6
Bob Gotchall <bob.g@sigmasense.com>
2022-02-17 15:19:31 -0600

    just some random experiments added to manipulate json in pandas

--- db_parser.py
+++ db_parser.py
@@ -1,63 +1,62 @@
 import sqlite3
 import json
+import pandas as pd
 from employee import Employee
 
    
 # Opening JSON file
-f = open('AthenaC_setup_20Aug2021_AAA_082321_CORR___20210902130347.json',)
+f = open('data/AthenaC_setup_20Aug2021_AAA_082321_CORR___20210902130347.json',)  #note that this still requires quick edit to export the json.
    
 # returns JSON object as 
 # a dictionary
-data = json.load(f)
+#data = json.load(f)
    
+
+df=pd.read_json('data/AthenaC_setup_20Aug2021_AAA_082321_CORR___20210902130347.json')
+
+print(df.head(100))
+print(df.tail(100))
+
+print(df['hand_id'])
+
 # Iterating through the json
 # list
-for i in data:
-    print(i)
+# for i in data:
+#     print(i)
    
 # Closing file
-f.close()
+#f.close()
 
 
 
 
 
-conn = sqlite3.connect('sample.db')
+# conn = sqlite3.connect('sample.db')
 
-c= conn.cursor()
-#c.execute("DROP TABLE employees")
+# c= conn.cursor()
+# #c.execute("DROP TABLE employees")
 
-#c.execute("""CREATE TABLE employees (
-#            first text,
-#            last text,
-#            pay integer
-#            )""")
+# #c.execute("""CREATE TABLE employees (
+# #            first text,
+# #            last text,
+# #            pay integer
+# #            )""")
 
-emp_1=Employee("John", "Doe", 80000)
-emp_2=Employee("Jane", "Doe", 90000)
+# emp_1=Employee("John", "Doe", 80000)
+# emp_2=Employee("Jane", "Doe", 90000)
 
-print(emp_1.first)
-print(emp_1.last)
-print(emp_1.pay)
-
-
-#c.execute("INSERT INTO employees VALUES (:first,:last, :pay)",{'first':emp_1.first,'last':emp_1.last,'pay':emp_1.pay})
-#conn.commit()
-
-c.execute("SELECT * FROM employees WHERE last='Doe'")
-
-print(c.fetchall())
+# print(emp_1.first)
+# print(emp_1.last)
+# print(emp_1.pay)
 
 
-conn.commit()
+# #c.execute("INSERT INTO employees VALUES (:first,:last, :pay)",{'first':emp_1.first,'last':emp_1.last,'pay':emp_1.pay})
+# #conn.commit()
 
+# c.execute("SELECT * FROM employees WHERE last='Doe'")
 
+# print(c.fetchall())
 
+# conn.commit()
 
-
-
-conn.close()
-
-
-
-print("hi")
+# conn.close()
--- pandas_reading_sqlite.py
+++ pandas_reading_sqlite.py
@@ -17,7 +17,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 lot_names=df.Lotid.unique()
-app = dash.Dash(__name__)
+#app = dash.Dash(__name__)
 #here I can start dumping in example displays with this simple data table
 
 
@@ -38,11 +38,75 @@ app.layout = html.Div([
 ])
 ###############################################################################
 # try a bar chart of say yields:
+###############################################################################
+# example sankey:
 
+# fig = go.Figure(data=[go.Sankey(
+#     node = dict(
+#       pad = 15,
+#       thickness = 20,
+#       line = dict(color = "black", width = 0.5),
+#       label = ["W1", "W2", "probe fail", "probe pass", "assy fail", "assy pass"],
+#       color = ["blue", "red"]
+#     ),
+#     link = dict(
+#       source = [0, 0,   1,    1   ,3     ,3], # indices correspond to labels, eg A1, A2, A1, B1, ...
+#       target = [2, 3,   2,    3   ,4     ,5],
+#       value = [50, 450, 40, 460   ,20    ,890 ]
+#   ))])
 
+# fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+# fig.show()
 
+import json
+import urllib
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+
+url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
+response = urllib.request.urlopen(url)
+data = json.loads(response.read())
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="graph"),
+    html.P("Opacity"),
+    dcc.Slider(id='opacity', min=0, max=1, 
+               value=0.5, step=0.1)
+])
+
+@app.callback(
+    Output("graph", "figure"), 
+    [Input("opacity", "value")])
+def display_sankey(opacity):
+    opacity = str(opacity)
+
+    # override gray link colors with 'source' colors
+    node = data['data'][0]['node']
+    link = data['data'][0]['link']
+
+    # Change opacity
+    node['color'] = [
+        'rgba(255,0,255,{})'.format(opacity) 
+        if c == "magenta" else c.replace('0.8', opacity) 
+        for c in node['color']]
+
+    link['color'] = [
+        node['color'][src] for src in link['source']]
+
+    fig = go.Figure(go.Sankey(link=link, node=node))
+    fig.update_layout(font_size=10)
+
+    return fig
 
 app.run_server(debug=True)
+
+
 
 
 
